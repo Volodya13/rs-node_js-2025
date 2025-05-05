afb234cb5af38038f59a10b83b458be33c18bc37
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-05-05 19:34:43 +0500

    feat: added navigation commands

--- file-manager/src/index.js
+++ file-manager/src/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import readline from 'readline';
 import os from 'os';
 import { chdir } from 'process';
+import { handleCd, handleLs, handleUp } from './navigation.js';
 
 const arg = process.argv.find(arg => arg.startsWith('--username='));
 const username = arg ? arg.split('=')[1] : null;
@@ -19,12 +20,33 @@ printCwd();
 
 rl.on('line', async (line) => {
 
-  if (line.trim() === '.exit') {
-    exitApp();
-  } else {
-    console.log('Invalid input');
-    printCwd();
+  const [command, ...args] = line.trim().split(' ');
+
+  try {
+    switch (command) {
+      case '.exit':
+        exitApp();
+        return;
+      case 'up':
+        handleUp();
+        break;
+      case 'cd':
+        if (args.length === 0) throw new Error('Missing path');
+        handleCd(args.join(' '));
+        break;
+      case 'ls':
+        handleLs();
+        break;
+      default:
+        console.log('Invalid input!');
+    }
+  } catch(error) {
+    console.log('Operation failed');
+    throw new Error(error);
   }
+
+  printCwd();
+  // rl.prompt();
 })
 
 process.on('SIGINT', () => exitApp());
