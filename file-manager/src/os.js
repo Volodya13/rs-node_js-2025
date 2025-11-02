e44ab2263ec90fd72995226f4038a51ecf2de031
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-11-02 22:59:57 +0500

    refactor: destructure imports of `os` methods

--- file-manager/src/os.js
+++ file-manager/src/os.js
@@ -1,25 +1,25 @@
-import os from 'os';
+import {EOL, homedir, arch, cpus, userInfo} from 'os';
 
 export function printEOL() {
-  console.log(JSON.stringify(os.EOL));
+  console.log(JSON.stringify(EOL));
 }
 
 export function printCPUs() {
-  const cpus = os.cpus();
-  console.log(`Total CPUs: ${cpus.length}`);
-  cpus.forEach((cpu, index) => {
+  console.log(`Total CPUs: ${cpus().length}`);
+	
+  cpus().forEach((cpu, index) => {
     console.log(`CPU ${index + 1}: ${cpu.model}, ${cpu.speed / 1000} GHz`);
   });
 }
 
 export function printHomeDir() {
-  console.log(`Home directory: ${os.homedir()}`);
+  console.log(`Home directory: ${homedir()}`);
 }
 
 export function printSystemUsername() {
-  console.log(os.userInfo().username);
+  console.log(userInfo().username);
 }
 
 export function printArchitecture() {
-  console.log(os.arch());
+  console.log(arch());
 }
